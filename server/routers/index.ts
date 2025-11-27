5c7d1421d79c57198cdb5b6a2dcf0b91214d5b66
Sandro3110 <234844223+Sandro3110@users.noreply.github.com>
2025-11-27 02:23:08 -0500

    fix: adicionar index.ts para exportar todos os routers explicitamente

---
+++ server/routers/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Routers Index - Export all routers
+ */
+
+export { appRouter, type AppRouter } from './_app';
+export { alertsRouter } from './alerts';
+export { analyticsRouter } from './analytics';
+export { authRouter } from './auth';
+export { dashboardRouter } from './dashboard';
+export { emailConfigRouter } from './emailConfig';
+export { enrichmentRouter } from './enrichment';
+export { exportRouter } from './export';
+export { geocodingRouter } from './geocoding';
+export { leadsRouter } from './leads';
+export { mercadosRouter } from './mercados';
+export { pesquisasRouter } from './pesquisas';
+export { projectsRouter } from './projects';
+export { reportsRouter } from './reports';
+export { territorialRouter } from './territorial';
+export { unifiedMapRouter } from './unifiedMap';
+export { usersRouter } from './users';
